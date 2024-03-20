8859b0cfe253eda42837152292da2f75427d3d24
Lakhwinder <Lakhwinder.chdit@gmail.com>
2024-03-20 22:15:49 +0530

    some changes in form

--- src/features/multi-addresses/multi-addresses.js
+++ src/features/multi-addresses/multi-addresses.js
@@ -1,7 +1,18 @@
 import { ComponentConstant } from "../../constants/ComponentConstant";
 export class MultiAdress{
     setHTML(){
-        return`<div>Hello it is working</div>`
+        return`<form id="multi-addresses">
+        <div class="container">
+                    <div class="row p-t20">
+                        <form-field-alpha-num class="col-md-6" label="Company Name" name="name"></form-field-alpha-num>
+                        <form-field-alpha-num class="col-md-6" label="Contact Email" name="email"></form-field-alpha-num>
+                    </div>
+                    <div class="row p-t20">
+                        <form-field-phone min="10" max="18" class="col-md-6" label="Phone" name="phone"></form-field-phone>
+                        <form-field-alpha-num class="col-md-6" label="Address" name="address"></form-field-alpha-num>
+                    </div></div>
+        </form>`
+        
     }
 }
 export default window.customElements.define(ComponentConstant.MULTIADDRESSESS, MultiAdress);
