4b5c23562353ee615b2efecd9c11f08097998c77
dev <dev@dev.com>
2024-03-21 16:27:56 +0530

    code pushed

--- src/features/multi-addresses/multi-addresses.js
+++ src/features/multi-addresses/multi-addresses.js
@@ -1,27 +1,61 @@
 import { ComponentConstant } from "../../constants/ComponentConstant.js";
 import { ListComponent } from "../../components/ListComponent.js";
 
-export class MultiAdress extends ListComponent {
+export class MultiAddress extends ListComponent {
   constructor() {
     super();
   }
 
   setHTML() {
-    return 
-    `<form id="multi-addresses">
-    <h2>Multi Address</h2>
-    <div class="container">
-                <div class="row p-t20">
-                    <form-field-alpha-num class="col-md-6" label="Company Name" name="name"></form-field-alpha-num>
-                    <form-field-alpha-num class="col-md-6" label="Contact Email" name="email"></form-field-alpha-num>
+    return `
+        <div class="container">
+            <div class="row border border-secondary rounded">
+                <div class="col-md-12">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 pt-2 text-left">
+                                <input style="float: right;" type="button"  class="btn btn-outline-secondary" >
+                            </div>
+                        </div>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Address 1</th>
+                                <th>Address 2</th>
+                                <th>City</th>
+                                <th>State</th>
+                                <th>Zip Code</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>123 Main St</td>
+                                <td>Apt 101</td>
+                                <td>New York</td>
+                                <td>NY</td>
+                                <td>10001</td>
+                            </tr>
+                            <tr>
+                                <td>456 Elm St</td>
+                                <td>Suite 200</td>
+                                <td>Los Angeles</td>
+                                <td>CA</td>
+                                <td>90001</td>
+                            </tr>
+                            <tr>
+                                <td>789 Oak St</td>
+                                <td>Unit B</td>
+                                <td>Chicago</td>
+                                <td>IL</td>
+                                <td>60601</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <div class="row p-t20">
-                    <form-field-phone min="10" max="18" class="col-md-6" label="Phone" name="phone"></form-field-phone>
-                    <form-field-alpha-num class="col-md-6" label="Address" name="address"></form-field-alpha-num>
-                </div></div>
-    </form>`
-    
+            </div>
+        </div>`;
   }
 }
 
-window.customElements.define(ComponentConstant.MULTIADDRESSES, MultiAdress);
+window.customElements.define(ComponentConstant.MULTIADDRESSES, MultiAddress);
