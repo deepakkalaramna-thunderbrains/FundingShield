70c3e3ac4a1c33509a67199a181e559c5781ff2d
dev <dev@dev.com>
2024-03-21 16:19:35 +0530

    made changes

--- src/features/multi-addresses/multi-addresses.js
+++ src/features/multi-addresses/multi-addresses.js
@@ -7,18 +7,18 @@ export class MultiAddress extends ListComponent {
   }
 
   setHTML() {
-    return;
-    ` <div class="container">
-                <div class="row border border-secondary rounded">
-                    <div class="col-md-12">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-12 pt-2 text-left">
-                                    <input style="float: right;" type="button" id="openAllDocuments" class="btn btn-outline-secondary" value="Open All Documents">
-                                </div>
+    return `
+        <div class="container">
+            <div class="row border border-secondary rounded">
+                <div class="col-md-12">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 pt-2 text-left">
+                                <input style="float: right;" type="button" id="openAllDocuments" class="btn btn-outline-secondary" value="Open All Documents">
                             </div>
                         </div>
-                        <table>
+                    </div>
+                    <table>
                         <thead>
                             <tr>
                                 <th>Address 1</th>
@@ -29,31 +29,32 @@ export class MultiAddress extends ListComponent {
                             </tr>
                         </thead>
                         <tbody>
-    <tr>
-        <td>123 Main St</td>
-        <td>Apt 101</td>
-        <td>New York</td>
-        <td>NY</td>
-        <td>10001</td>
-    </tr>
-    <tr>
-        <td>456 Elm St</td>
-        <td>Suite 200</td>
-        <td>Los Angeles</td>
-        <td>CA</td>
-        <td>90001</td>
-    </tr>
-    <tr>
-        <td>789 Oak St</td>
-        <td>Unit B</td>
-        <td>Chicago</td>
-        <td>IL</td>
-        <td>60601</td>
-    </tr>
-</tbody>
-
+                            <tr>
+                                <td>123 Main St</td>
+                                <td>Apt 101</td>
+                                <td>New York</td>
+                                <td>NY</td>
+                                <td>10001</td>
+                            </tr>
+                            <tr>
+                                <td>456 Elm St</td>
+                                <td>Suite 200</td>
+                                <td>Los Angeles</td>
+                                <td>CA</td>
+                                <td>90001</td>
+                            </tr>
+                            <tr>
+                                <td>789 Oak St</td>
+                                <td>Unit B</td>
+                                <td>Chicago</td>
+                                <td>IL</td>
+                                <td>60601</td>
+                            </tr>
+                        </tbody>
                     </table>
-            </div>`;
+                </div>
+            </div>
+        </div>`;
   }
 }
 
