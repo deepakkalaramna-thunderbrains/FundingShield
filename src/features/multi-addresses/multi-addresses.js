a961854f93a4dc9f0daa458171177f7efa99f7fc
dev <dev@dev.com>
2024-03-21 16:08:31 +0530

    changes committed

--- src/features/multi-addresses/multi-addresses.js
+++ src/features/multi-addresses/multi-addresses.js
@@ -18,34 +18,20 @@ export class MultiAddress extends ListComponent {
                                 </div>
                             </div>
                         </div>
-                        <table class="table table-striped">
-                            <thead class="table-light text-center">
-                                <tr>
-                                    <th>address 1</th>
-                                    <th>address 2</th>
-                                    <th>city</th>
-                                    <th>state</th>
-                                    <th>zip Code</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </table>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <input style="float: left;" type="button" id="firstReviewReceived" class="btn btn-outline-secondary" value="1st Review Received">
-                            </div>
-                            <div class="col-md-8">
-                                <input style="float: left;" type="button" id="validateAllFirst" class="btn btn-outline-secondary" value="Validate First Reviewer">
-                                <input style="float: right;" type="button" id="validateAll" class="btn btn-outline-secondary" value="Validate Second Reviewer">
-                            </div>
-                        </div>
-                        <br>
-                    </div>
-                </div>
-                <div class="row border border-secondary rounded m-t30 p-a20">
-                    <document-upload style="width: 100%"></document-upload>
-                </div>
+                        <table>
+                        <thead>
+                            <tr>
+                                <th>Address 1</th>
+                                <th>Address 2</th>
+                                <th>City</th>
+                                <th>State</th>
+                                <th>Zip Code</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Table rows will be added with actual data -->
+                        </tbody>
+                    </table>
             </div>`;
     }
 }
