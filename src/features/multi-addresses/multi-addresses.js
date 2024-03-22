865e2ab83c70097949b4b07a395bead68055cc41
dev <dev@dev.com>
2024-03-22 12:13:22 +0530

    data binded

--- src/features/multi-addresses/multi-addresses.js
+++ src/features/multi-addresses/multi-addresses.js
@@ -2,63 +2,75 @@ import { ComponentConstant } from "../../constants/ComponentConstant.js";
 import { ListComponent } from "../../components/ListComponent.js";
 
 export class MultiAddress extends ListComponent {
-  constructor() {
+  constructor(addresses) {
     super();
+    this.addresses = addresses || [
+      {
+        address1: "123 Main St",
+        address2: "Apt 101",
+        city: "New York",
+        state: "NY",
+        zipCode: "10001"
+      },
+      {
+        address1: "456 Elm St",
+        address2: "Suite 200",
+        city: "Los Angeles",
+        state: "CA",
+        zipCode: "90001"
+      },
+      {
+        address1: "789 Oak St",
+        address2: "Unit B",
+        city: "Chicago",
+        state: "IL",
+        zipCode: "60601"
+      }
+    ];
   }
 
   setHTML() {
-    return` <div class="container mt-4">
-    <div class="row border border-secondary rounded p-3">
-        <div class="col-md-12">
-            <div class="row align-items-center mb-3">
-                <div class="col-md-6">
-                    <input type="text" id="searchInput" class="form-control" placeholder="Search...">
+    const addressRows = this.addresses.map(address => `
+      <tr>
+        <td>${address.address1}</td>
+        <td>${address.address2}</td>
+        <td>${address.city}</td>
+        <td>${address.state}</td>
+        <td>${address.zipCode}</td>
+      </tr>
+    `).join('');
+
+    return `<div class="container mt-4">
+        <div class="row border border-secondary rounded p-3">
+            <div class="col-md-12">
+                <div class="row align-items-center mb-3">
+                    <div class="col-md-6">
+                        <input type="text" id="searchInput" class="form-control" placeholder="Search...">
+                    </div>
+                    <div class="col-md-6 text-md-right mt-2 mt-md-0">
+                        <button type="button" id="searchButton" class="btn btn-primary">
+                            <i class="bi bi-search"></i> Search
+                        </button>
+                    </div>
                 </div>
-                <div class="col-md-6 text-md-right mt-2 mt-md-0">
-                    <button type="button" id="searchButton" class="btn btn-primary">
-                        <i class="bi bi-search"></i> Search
-                    </button>
-                </div>
+
+                <table class="table table-striped">
+                    <thead class="thead-light text-center"> 
+                        <tr>
+                            <th>Address 1</th>
+                            <th>Address 2</th>
+                            <th>City</th>
+                            <th>State</th>
+                            <th>Zip Code</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${addressRows}
+                    </tbody>
+                </table>
             </div>
-
-            <table class="table table-striped">
-                <thead class="thead-light text-center"> 
-                    <tr>
-                        <th>Address 1</th>
-                        <th>Address 2</th>
-                        <th>City</th>
-                        <th>State</th>
-                        <th>Zip Code</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>123 Main St</td>
-                        <td>Apt 101</td>
-                        <td>New York</td>
-                        <td>NY</td>
-                        <td>10001</td>
-                    </tr>
-                    <tr>
-                        <td>456 Elm St</td>
-                        <td>Suite 200</td>
-                        <td>Los Angeles</td>
-                        <td>CA</td>
-                        <td>90001</td>
-                    </tr>
-                    <tr>
-                        <td>789 Oak St</td>
-                        <td>Unit B</td>
-                        <td>Chicago</td>
-                        <td>IL</td>
-                        <td>60601</td>
-                    </tr>
-                </tbody>
-            </table>
         </div>
-    </div>
-</div>`;
-
+    </div>`;
   }
 }
 
